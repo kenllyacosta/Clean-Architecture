4256608eca091d24ef369b96882e43b93b90bc54
Kenlly Acosta <kenllyacosta@hotmail.com>
2021-09-17 20:37:50 -0400

    Repository interfaces edited

--- Domain/Interfaces/ICountRepository.cs
+++ Domain/Interfaces/ICountRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace Domain.Interfaces
 {
     /// <summary>
-    /// Esta interface sirve paracuando necesitamos obtener la cantidad de registros de una tabla
+    /// Esta interface sirve para cuando necesitamos obtener la cantidad de registros de una tabla
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public interface ICountRepository<T> where T : class
